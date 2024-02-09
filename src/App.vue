dda1162953c6875fdab879c8fe132c0e5ad92d31
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-02-09 21:18:08 +0100

    Started migration of frontend to vuetify

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,26 @@ let toolsOpened = ref(true)
 </script>
 <
 <template>
-  <v-card>
-    <v-layout>
-      <v-btn @click="modelsOpened = !modelsOpened" max-width="22px" style="position:absolute; z-index: 1">&gt;</v-btn>
-      <v-navigation-drawer v-model="modelsOpened">
-        <v-list density="compact" nav > <!-- TODO: Accordion -->
-          <v-list-item-title>Models <v-btn @click="modelsOpened = !modelsOpened" style="float: right" height="22px"><</v-btn></v-list-item-title>
-          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main style="height: 100vh">
-        <ModelViewer/>
-      </v-main>
-      <v-navigation-drawer location="right" v-model="toolsOpened" opa>
-        <v-list density="compact" nav >
-          <v-list-item-title>Tools</v-list-item-title>
-          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-layout>
-  </v-card>
+  <v-layout>
+    <v-btn @click="modelsOpened = !modelsOpened" max-width="22px" style="position:absolute; z-index: 1">&gt;</v-btn>
+    <v-navigation-drawer v-model="modelsOpened">
+      <v-list density="compact" nav > <!-- TODO: Accordion -->
+        <v-list-item-title>Models <v-btn @click="modelsOpened = !modelsOpened" style="float: right" height="22px"><</v-btn></v-list-item-title>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main style="height: 100vh">
+      <ModelViewer/>
+    </v-main>
+    <v-navigation-drawer location="right" v-model="toolsOpened" opa>
+      <v-list density="compact" nav >
+        <v-list-item-title>Tools</v-list-item-title>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 
 <style scoped>
