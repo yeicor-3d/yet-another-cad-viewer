258256912b04a86ccbb68731ecff67107f13330d
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-03-03 20:34:52 +0100

    optimize CI times

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-    # When building the backend, make sure the frontend is built first
-    subprocess.run(['yarn', 'install'], check=True)
-    subprocess.run(['yarn', 'build', '--outDir', 'yacv_server/frontend'], check=True)
+    if os.getenv('SKIP_BUILD_FRONTEND') is None:
+        # When building the backend, make sure the frontend is built first
+        subprocess.run(['yarn', 'install'], check=True)
+        subprocess.run(['yarn', 'build', '--outDir', 'yacv_server/frontend'], check=True)
