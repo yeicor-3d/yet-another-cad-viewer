682f41d3f8b71eb345e1779ecd58a9f2737a6a86
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-03-03 20:22:08 +0100

    fix frontend to use relative urls 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: "",
     plugins: [
         vue({
             template: {
