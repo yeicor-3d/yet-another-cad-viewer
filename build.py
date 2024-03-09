8b3d08826deef4b17c8654291f67a17ce994ab18
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-03-09 11:47:23 +0100

    small fix for pip

--- build.py
+++ build.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-    if os.getenv('SKIP_BUILD_FRONTEND') is None:
+    # Building the frontend is optional
+    if os.getenv('SKIP_BUILD_FRONTEND') is None and os.path.exists('package.json'):
         # When building the backend, make sure the frontend is built first
         subprocess.run(['yarn', 'install'], check=True)
         subprocess.run(['yarn', 'build', '--outDir', 'yacv_server/frontend'], check=True)
